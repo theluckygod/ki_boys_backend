7d493d2da8c7901013f62ea8fea895df3b51a52b
thieunv <thieunv@vng.com.vn>
2023-08-12 02:29:24 +0700

    authorize

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Union, List
 from typing_extensions import Annotated
 import requests
 
-from fastapi import FastAPI, Header, Depends, HTTPException
+from fastapi import FastAPI, Header, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 
 import crud
@@ -14,7 +14,7 @@ from database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+AUTHORIZED_TOKENS = {}
 
 # Dependency
 def get_db():
@@ -23,41 +23,57 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+def verify_token(req: Request):
+    token = req.headers["token"]
+    if token not in AUTHORIZED_TOKENS:
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized"
+        )
+    return AUTHORIZED_TOKENS[token]
+
 
 @app.get("/users/me")
 async def get_me(token: Annotated[str, Header()]):
-    # response = requests.get(url="https://graph.zalo.me/v2.0/me",
-    #                         headers={"access_token": token})
+    if token not in AUTHORIZED_TOKENS:
+        # response = requests.get(url="https://graph.zalo.me/v2.0/me",
+        #                         headers={"access_token": token})
     
-    response = {
-        "is_sensitive": False,
-        "name": "Tùng Nguyễn",
-        "id": "3681046936240438345",
-        "error": 0,
-        "message": "Success",
-        "picture": {
-            "data": {
-                "url": "https://s120-ava-talk.zadn.vn/a/a/6/e/37/120/84f0ddd1d0f1edf0831c92cf4960e3ec.jpg"
+        response = {
+            "is_sensitive": False,
+            "name": "Tùng Nguyễn",
+            "id": "3681046936240438345",
+            "error": 0,
+            "message": "Success",
+            "picture": {
+                "data": {
+                    "url": "https://s120-ava-talk.zadn.vn/a/a/6/e/37/120/84f0ddd1d0f1edf0831c92cf4960e3ec.jpg"
+                }
             }
         }
-    }
+        
+        AUTHORIZED_TOKENS[token] = response
+    else:
+        response = AUTHORIZED_TOKENS[token]
+
     return response
 
 
 @app.get("/users/", response_model=List[schemas.User])
-async def read_users(token: Annotated[str, Header()], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(user_info: dict = Depends(verify_token), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/items/", response_model=List[schemas.Item])
-def read_items(token: Annotated[str, Header()], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items(user_info: dict = Depends(verify_token), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
 
 
 @app.get("/subcriptions/", response_model=List[schemas.Subcription])
-def read_subcriptions(token: Annotated[str, Header()], skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_subcriptions(user_info: dict = Depends(verify_token), skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     subcriptions = crud.get_subcriptions(db, skip=skip, limit=limit)
     return subcriptions
 
