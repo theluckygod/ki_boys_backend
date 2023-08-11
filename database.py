a6ec689425691e9f85cc6b85a785268339242e98
thieunv <thieunv@vng.com.vn>
2023-08-11 22:22:10 +0700

    fix bug: mysql cannot connect

--- database.py
+++ database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "mysql://db_team_05:ubN#1aF8M!S3@118.102.2.130:33306/db_team_05"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
